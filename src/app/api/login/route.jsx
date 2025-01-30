5925074a32b16cf01c5db0cbf15de48917467f74
Rob Killick <robkillick@Robs-MBP-2.home>
2025-01-30 13:55:15 +0000

    implement basic login functionality with JWT

---
+++ src/app/api/login/route.jsx
@@ -0,0 +1,33 @@
+"use server"
+
+import { getRefreshToken, getToken, setRefreshToken, setToken } from "@/app/lib/auth"
+import { cookies } from "next/headers"
+import { NextResponse } from "next/server"
+
+const DJANGO_API_LOGIN_URL = "http://127.0.0.1:8001/api/token/pair"
+
+export async function POST(request) {
+  const myAuthToken = getToken
+  const myRefreshToken = getRefreshToken
+  console.log(myAuthToken, myRefreshToken )
+
+  const requestData = await request.json()
+  const jsonData = JSON.stringify(requestData)
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: jsonData
+  }
+  const response = await fetch(DJANGO_API_LOGIN_URL, requestOptions)
+  const responseData = await response.json()
+  if (response.ok) {
+    console.log('logged in')
+    const {access, refresh}  = responseData
+    setToken(access)
+    setRefreshToken(refresh)
+  }
+
+  return NextResponse.json({"hello": "world"}, {status: 200})
+}
---
+++ src/app/login/page.jsx
@@ -0,0 +1,38 @@
+"use client"
+
+const LOGIN_URL = "/api/login/"
+
+export default function Page() {
+
+  async function handleSubmit (event) {
+    event.preventDefault()
+    console.log(event, event.target)
+    const formData = new FormData(event.target)
+        const objectFromForm = Object.fromEntries(formData)
+        const jsonData = JSON.stringify(objectFromForm)
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: jsonData
+        }
+        const response = await fetch(LOGIN_URL, requestOptions)
+        // const data = await response.json()
+        if (response.ok) {
+            console.log("logged in")
+        }
+  }
+  return(
+    <div className="h-[95vh]">
+      <div className="max-w-md mx-auto py-5">
+      <h1>login here</h1>
+      <form onSubmit={handleSubmit}>
+          <input type="email" required name='email' placeholder='Your email' />
+          <input type="password" required name='password' placeholder='Your password' />
+          <button type="submit">Login</button>
+      </form>
+      </div>
+    </div>
+  )
+}
