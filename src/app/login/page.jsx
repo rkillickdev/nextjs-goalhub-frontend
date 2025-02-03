79b2cb5620d34815529f4421b1050bdfc1c175c8
Rob Killick <robkillick@Robs-MBP-2.home>
2025-02-03 13:18:04 +0000

    use shad cn login template and modify for login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,19 @@
 "use client"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import { useAuth } from "@/components/authProvider"
+
 const LOGIN_URL = "/api/login/"
 
-export default function Page() {
+export default function Page({
+  className,
+  ...props
+}) {
   const auth = useAuth()
-
   async function handleSubmit (event) {
     event.preventDefault()
     console.log(event, event.target)
@@ -26,15 +35,59 @@ export default function Page() {
             auth.login()
         }
   }
-  return(
-    <div className="h-[95vh]">
-      <div className="max-w-md mx-auto py-5">
-      <h1>login here</h1>
-      <form onSubmit={handleSubmit}>
-          <input type="email" required name='email' placeholder='Your email' />
-          <input type="password" required name='password' placeholder='Your password' />
-          <button type="submit">Login</button>
-      </form>
+return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form onSubmit={handleSubmit} className="p-6 md:p-8">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">Welcome back</h1>
+                <p className="text-balance text-muted-foreground">
+                  Login to your Acme Inc account
+                </p>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" name="password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+              <div className="grid grid-cols-3 gap-4">
+              </div>
+              <div className="text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <a href="#" className="underline underline-offset-4">
+                  Sign up
+                </a>
+              </div>
+            </div>
+          </form>
+          <div className="relative hidden bg-muted md:block">
+            <img
+              src="/placeholder.svg"
+              alt="Image"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
       </div>
     </div>
   )
